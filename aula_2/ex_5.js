f8669e445744d37e216afadfc5df3a36002e26da
GabrielArrudaDev <gabrielarruda321@gmail.com>
2023-08-26 15:11:43 -0300

    aula_2_exercicios_resolvidos

--- aula_2/ex_5.js
+++ aula_2/ex_5.js
@@ -1,5 +1,5 @@
 /**
-Escreva uma função que recebe um vetor de números inteiros e mostre como
+screva uma função que recebe um vetor de números inteiros e mostre como
 resultado qual é o menor e qual é o maior número.
  */
 
